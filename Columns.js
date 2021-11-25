e2fbe9a1f622e8bb1be03c56e52748e7c01df15d
abhi123654 <65011919+abhi123654@users.noreply.github.com>
2021-11-25 14:55:30 +0530

    Create Columns.js

---
+++ Columns.js
@@ -0,0 +1,27 @@
+export const COLUMNS = [
+    {
+        Header: 'EmployeeID',
+        Footer: 'EmployeeID',
+        accessor: 'id',
+    },
+    {
+        Header: 'FirstName',
+        Footer: 'FirstName',
+        accessor: 'firstName',
+    },
+    {
+        Header: 'LastName',
+        Footer: 'LastName',
+        accessor: 'lastName',
+    },
+    {
+        Header: 'Age',
+        Footer: 'Age',
+        accessor: 'age',
+    },
+    {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor: 'email',
+    }
+]
